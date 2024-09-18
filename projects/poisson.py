a51d0aff2b8bc89d63c0d4a576b0c3b51192c6de
AnthonyTSV <mrantoshkas@gmail.com>
2024-09-18 10:55:55 +0200

    poisson, poisson2d solvers

--- projects/poisson.py
+++ projects/poisson.py
@@ -113,16 +113,22 @@ class Poisson:
         return np.sqrt(self.dx*np.sum((uj-u)**2))
 
 def test_poisson():
-    assert False
-
-if __name__ == '__main__':
     L = 2
     sol = Poisson(L=L)
     ue = sp.exp(4*sp.cos(x))
-    #ue = x**2
     bc = (ue.subs(x, 0), ue.subs(x, L))
     u = sol(100, bc=bc, f=sp.diff(ue, x, 2))
-    print('Manufactured solution: ', ue)
-    print(f'Boundary conditions: u(0)={bc[0]:2.4f}, u(L)={bc[1]:2.2f}')
-    print(f'Discretization: N = {sol.N}')
-    print(f'L2-error {sol.l2_error(u, ue)}')
+    assert sol.l2_error(u, ue) < 1e-2
+
+if __name__ == '__main__':
+    # L = 2
+    # sol = Poisson(L=L)
+    # ue = sp.exp(4*sp.cos(x))
+    # #ue = x**2
+    # bc = (ue.subs(x, 0), ue.subs(x, L))
+    # u = sol(100, bc=bc, f=sp.diff(ue, x, 2))
+    # print('Manufactured solution: ', ue)
+    # print(f'Boundary conditions: u(0)={bc[0]:2.4f}, u(L)={bc[1]:2.2f}')
+    # print(f'Discretization: N = {sol.N}')
+    # print(f'L2-error {sol.l2_error(u, ue)}')
+    test_poisson()
--- projects/poisson2d.py
+++ projects/poisson2d.py
@@ -15,22 +15,43 @@ class Poisson2D:
     with homogeneous Dirichlet boundary conditions.
     """
 
-    def __init__(self, Lx, Ly, Nx, Ny):
+    def __init__(self, Lx, Ly, Nx, Ny, ue):
         self.px = Poisson(Lx, Nx) # we can reuse some of the code from the 1D case
         self.py = Poisson(Ly, Ny)
+        self.xij = None
+        self.yij = None
+        self.ue = ue
 
     def create_mesh(self):
         """Return a 2D Cartesian mesh
         """
-        raise NotImplementedError
+        return np.meshgrid(self.px.x, self.py.x)
 
     def laplace(self):
         """Return a vectorized Laplace operator"""
-        raise NotImplementedError
+        D2x = self.px.D2()
+        D2y = self.py.D2()
+        return (sparse.kron(D2x, sparse.eye(self.py.N + 1)) +
+            sparse.kron(sparse.eye(self.px.N + 1), D2y))
 
     def assemble(self, f=None):
         """Return assemble coefficient matrix A and right hand side vector b"""
-        raise NotImplementedError
+        xij, yij = self.create_mesh()
+        self.xij = xij
+        self.yij = yij
+        A = self.laplace()
+        F = sp.lambdify((x, y), f)(xij, yij)
+        B = np.ones((self.px.N + 1, self.py.N + 1), dtype=bool)
+        B[1:-1, 1:-1] = 0
+        bnds = np.where(B.ravel() == 1)[0]
+        A = A.tolil()
+        for i in bnds:
+            A[i] = 0
+            A[i, i] = 1
+        A = A.tocsr()
+        b = F.ravel()
+        b[bnds] = sp.lambdify((x, y), self.ue)(self.xij, self.yij).ravel()[bnds]
+        return A, b.ravel()
 
     def l2_error(self, u, ue):
         """Return l2-error
@@ -42,7 +63,9 @@ class Poisson2D:
         ue : Sympy function
             The analytical solution
         """
-        raise NotImplementedError
+        dx = self.px.dx
+        dy = self.py.dx
+        return np.sqrt(dx*dy*np.sum((u - sp.lambdify((x, y), ue)(self.xij, self.yij))**2))
 
     def __call__(self, f=implemented_function('f', lambda x, y: 2)(x, y)):
         """Solve Poisson's equation with a given righ hand side function
@@ -63,5 +86,19 @@ class Poisson2D:
         return sparse.linalg.spsolve(A, b.ravel()).reshape((self.px.N+1, self.py.N+1))
 
 def test_poisson2d():
-    assert False
+    Lx = 1
+    Ly = 1
+    Nx = 30
+    Ny = 30
+    ue = x*(1-x)*y*(1-y)*sp.exp(sp.cos(4*sp.pi*x)*sp.sin(2*sp.pi*y))
+    f = ue.diff(x, 2) + ue.diff(y, 2)
+    sol = Poisson2D(Lx, Ly, Nx, Ny, ue)
+    u = sol(f)
+    xij, yij = sol.xij, sol.yij
+    plt.contourf(xij, yij, u)
+    plt.colorbar()
+    plt.show()
+    assert sol.l2_error(u, ue) < 1e-3
 
+if __name__ == "__main__":
+    test_poisson2d()
